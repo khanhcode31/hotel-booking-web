481d1f627d5c7332ed9c7643d589cec379b50ef5
khanhcode31 <tnkhanh312006@gmail.com>
2026-05-10 13:42:10 +0700

    xoa cot ngay sinh

--- database.sql
+++ database.sql
@@ -6,7 +6,6 @@ CREATE TABLE users(
     email VARCHAR(255) NOT NULL UNIQUE,
     sdt VARCHAR(20) NOT NULL,
     address VARCHAR(255) NOT NULL,
-    birthday DATE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
